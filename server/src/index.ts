35db0c72f4e708777d14766cf29e9a89ae6e473f
Stratienko <artemstratienko94@gmail.com>
2024-09-29 21:32:59 +0100

    feat: code cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { createServer } from 'express-zod-api';
 import { routing } from '@/routing';
-import { config } from '@/config/v1';
+import { config } from '@/config';
 
 dotenv.config();
 
--- server/src/utils/common/pick-random-from.ts
+++
@@ -1,5 +0,0 @@
-export const pickRandomFrom = <T>(array: T[]) => {
-  const index = Math.floor(Math.random() * array.length);
-
-  return array.at(index) as T;
-};
